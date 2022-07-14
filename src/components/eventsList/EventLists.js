3c77bd675f694fe1d4fc6d798d324f071cdde199
Hiena <aledaldin@gmail.com>
2022-07-14 21:36:29 +0200

    eventlist with get data from database not working

--- src/components/eventsList/EventLists.js
+++ src/components/eventsList/EventLists.js
@@ -1,24 +1,30 @@
 // React
+import { useState, useEffect } from "react";
+// Firebase
+import { db } from "../firebase/firebase";
+import { collection, getDocs, query } from "firebase/firestore";
 
 function EventsList() {
   //******STATES*/
   const [events, setEvents] = useState([]);
 
   //******USE EFFECT*/
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
   //******LOGIC*/
-  // async function getData() {
-  //   const q = query(collection(db, "eventos"));
-  //   const querySnapshot = await getDocs(q);
-  //   console.log("db", querySnapshot);
-  //   querySnapshot.forEach((doc) => {
-  //     console.log(`${doc.id} => ${doc.data()}`);
-  //     //   doc.data.value.mapValue.fields.description
-  //   });
-  // }
+  async function getData() {
+    const q = query(collection(db, "eventos"));
+    const querySnapshot = await getDocs(q);
+    console.log("db", querySnapshot);
+    querySnapshot.forEach((doc) => {
+      let datosMolones = doc.data();
+      console.log("eeee", datosMolones);
+      console.log(`${doc.id}: ${doc.data().description}`);
+      //   doc.data.value.mapValue.fields.description
+    });
+  }
 
   return <p>aquí listado de eventos</p>;
 }
